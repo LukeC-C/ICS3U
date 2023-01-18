dda0f92e37df4adcb1248af4ce577b6eaae151e2
Luke C-C <luke.cornboy@gmail.com>
2023-01-17 22:36:57 -0500

    Replaced the java cup with my own icon, kept working on blocking.

--- Chess/game/Rook.java
+++ Chess/game/Rook.java
@@ -14,61 +14,105 @@ public class Rook extends Peice {
     @Override
     public boolean movePeice(int dX, int dY, int cX, int cY) {
         System.out.println("Wow, you moved a rook!");
+        Peice[] arraySlice = new Peice[8];
 
-        // I realize how terrible this code is, but I don't feel like fixing it right now
-        for (int i = 0; i < peiceArr.length; i++) {
-            for (int j = 0; j < peiceArr.length; j++) {
-                if (dX == cX) {
-                    // System.out.println("got to first step");
-                    if (dX < cX) {
-                        if (j == dY && i > dX && i < cX) {
-                            if (!(peiceArr[i][j] instanceof Empty)) {
-                                System.out.println("nopers");
-                                return false;
-                            }
-                        } else {
-                            return true;
-                        }
-                    } else {
-
-                        // TODO Here V V V
-                        // System.out.println("got to second step");
-                        // System.out.println("j: " + j + " dY: " + dY + " i: " + i + " cX: " + cX + " dX " + dX);
-                        if (j == dY && i > cX && i < dX) {
-                            // System.out.println("got to third step");
-                            if (!(peiceArr[i][j] instanceof Empty)) {
-                                // System.out.println("got to fourth step");
-                                System.out.println("nopers");
-                                return false;
-                            }
-                        }
-                        // this one ^^^^^
-
-                    }
-                } else if (dY == cY) { 
-                    if (dY < cY) {
-                        if (i == dY && i > dY && i < cY) {
-                            if (!(peiceArr[i][j] instanceof Empty)) {
-                                System.out.println("nopers");
-                                return false;
-                            }
-                        } else {
-                            System.out.println();
-                            return true;
-                        }
-                    } else {
-                        if (i == dY && i > cY && i < dY) {
-                            if (!(peiceArr[i][j] instanceof Empty)) {
-                                System.out.println("nopers");
-                                return false;
-                            }
-                        }
-                    }
-                } else {
-                    return false;
-                }
+        if ((dX == cX && dY != cY)) {
+            for (int i = peiceArr.length - 1; i > -1; i--) {
+                arraySlice[i] = peiceArr[cX - i][cY];
             }
+            // if (dY < cY) {
+                
+                for (int i = dY; i < cY; i++) {
+                    System.out.println(arraySlice[i]);
+                    if (!(arraySlice[i] instanceof Empty)) {
+                        if (!(arraySlice[i] instanceof Rook)) {
+                            System.out.println("nopers");
+                            return false;
+                        } else {
+                            if (!(i == cX)) {
+                                System.out.println(arraySlice[i] + " " + i + " " + cX);
+                                return false;
+                            } else {
+                                System.out.println(arraySlice[i] + " " + i + " " + cX);
+                            }
+                        }
+                    }
+                }
+                // WHAT I NEED TO DO:
+                    // make it so that i detects one less than the 
+
+            // } else {
+            //     for (int i = cY; i < dY; i++) {
+            //         if (!(arraySlice[i] instanceof Empty)) {
+            //             System.out.println("nopers");
+            //             return false;
+            //         }
+            //     }
+            // }
+            return true;
+        } else if ((dY == cY && dX != cX)){
+
+            return true; 
+        } else {
+            return false;
         }
-        return true;
+
+    //     // I realize how terrible this code is, but I don't feel like fixing it right now
+    //     for (int i = 0; i < peiceArr.length; i++) {
+    //         for (int j = 0; j < peiceArr.length; j++) {
+    //             if (dX == cX) {
+    //                 // System.out.println("got to first step");
+    //                 if (dX < cX) {
+    //                     if (j == dY && i > dX && i < cX) {
+
+    //                         if (!(peiceArr[i][j] instanceof Empty)) {
+    //                             System.out.println("nopers");
+    //                             return false;
+    //                         }
+    //                     } else {
+    //                         return true;
+    //                     }
+    //                 } else {
+
+    //                     // Here V V V
+    //                     // System.out.println("got to second step");
+    //                     // System.out.println("j: " + j + " dY: " + dY + " i: " + i + " cX: " + cX + " dX " + dX);
+    //                     if (i == dY && (j > cX && j < dX) || (j < cX && j > dX)) {
+    //                         System.out.println("got to third step");
+    //                         if (!(peiceArr[j][i] instanceof Empty)) {
+    //                             // System.out.println("got to fourth step");
+    //                             System.out.println("nopers");
+    //                             return false;
+    //                         }
+    //                     }
+    //                     // this one ^^^^^
+
+    //                 }
+    //             } else if (dY == cY) { 
+    //                 if (dY < cY) {
+    //                     if (i == dY && i > dY && i < cY) {
+    //                         if (!(peiceArr[i][j] instanceof Empty)) {
+    //                             System.out.println("nopers");
+    //                             return false;
+    //                         }
+    //                     } else {
+    //                         System.out.println();
+    //                         return true;
+    //                     }
+    //                 } else {
+    //                     if (i == dY && i > cY && i < dY) {
+    //                         if (!(peiceArr[i][j] instanceof Empty)) {
+    //                             System.out.println("nopers");
+    //                             return false;
+    //                         }
+    //                     }
+    //                 }
+    //             } else {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
     }
 }
